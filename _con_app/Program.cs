bb749f6b2de77aac8b16e8b87e5df96a1bc5c728
isotop786 <marufislam77@gmail.com>
2022-08-19 18:30:32 +0600

    Add project files.

---
+++ _con_app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _con_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
